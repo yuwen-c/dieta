e58a837e53eaf53312d719ae97c30e366d968cfb
marine <marine@MarinedeiMac.local>
2020-08-10 12:02:19 +0800

    bind labels with options

--- src/Options.js
+++ src/Options.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 const Options = ({Pname}) => {
+  // have to fix: correspond label to options
+  // The for attribute of <label> must be equal to the id attribute of the related element 
     return(
         <div>
             <div className="flex items-center mb2">
-              <input className="mr2" type="radio" id="Low" value="Low" name={Pname}/>
-              <label htmlFor="spacejam" className="lh-copy">Low</label>
+              <input className="mr2" type="radio" id={`${Pname}-low`} value="Low" name={Pname}/>
+              <label htmlFor={`${Pname}-low`} className="lh-copy">Low</label>
             </div>
             <div className="flex items-center mb2">
-              <input className="mr2" type="radio" id="Middle" value="Middle" name={Pname}/>
-              <label htmlFor="airbud" className="lh-copy">Middle</label>
+              <input className="mr2" type="radio" id={`${Pname}-middle`} value="Middle" name={Pname}/>
+              <label htmlFor={`${Pname}-middle`} className="lh-copy">Middle</label>
             </div>
             <div className="flex items-center mb2">
-              <input className="mr2" type="radio" id="High" value="High" name={Pname}/>
-              <label htmlFor="hocuspocus" className="lh-copy">High</label>
+              <input className="mr2" type="radio" id={`${Pname}-high`} value="High" name={Pname}/>
+              <label htmlFor={`${Pname}-high`} className="lh-copy">High</label>
             </div>
         </div>
     )
