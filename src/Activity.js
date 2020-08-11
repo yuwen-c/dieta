3783f31bf904387d0ec126fed3860dbfd74c508d
marine <marine@MarinedeiMac.local>
2020-08-11 09:20:30 +0800

    change layout of exercise and activity

--- src/Activity.js
+++ src/Activity.js
@@ -5,25 +5,24 @@ const Activity = () => {
     const weekArr = ['Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     return(
         <div>
-            <div className="pa3 pa5-ns">
-                <h4 className="f6 fw6">amount of activity:</h4>
-                <dl className="f6 lh-title mv2">
-                  <dt className="dib b">Low:</dt>
-                  <dd className="dib ml0 gray">6000 steps, or 30mins mounting/climbing stairs, or 1hr walking, house cleaning/biking.</dd>
-                </dl>
-                <dl className="f6 lh-title mv2">
-                  <dt className="dib b">Middle:</dt>
-                  <dd className="dib ml0 gray">10000 steps, or 1hr mounting/climbing stairs, or 2hr walking, house cleaning/biking.</dd>
-                </dl>
-                <dl className="f6 lh-title mv2">
-                  <dt className="dib b">High:</dt>
-                  <dd className="dib ml0 gray">20000 steps, or 2hr mounting/climbing stairs, or 3hr walking, house cleaning/biking.</dd>
-                </dl>
-            </div>
-
             <fieldset id="favorite_movies" className="bn">
-              <legend className="fw7 f4 ">choose your amount of activity this week</legend>
-                <div>
+              <legend className="fw7 f4 ">choose your amount of activity this week</legend>      
+                <div className="">
+                    <dl className="f6 lh-title mv2">
+                      <dt className="dib b">Low:</dt>
+                      <dd className="dib ml0 mid-gray">6000 steps, or 30mins mounting/climbing stairs, or 1hr walking, house cleaning/biking.</dd>
+                    </dl>
+                    <dl className="f6 lh-title mv2">
+                      <dt className="dib b">Middle:</dt>
+                      <dd className="dib ml0 mid-gray">10000 steps, or 1hr mounting/climbing stairs, or 2hr walking, house cleaning/biking.</dd>
+                    </dl>
+                    <dl className="f6 lh-title mv2">
+                      <dt className="dib b">High:</dt>
+                      <dd className="dib ml0 mid-gray">20000 steps, or 2hr mounting/climbing stairs, or 3hr walking, house cleaning/biking.</dd>
+                    </dl>
+                </div>
+
+            <div>
                 {
                     weekArr.map(item => {
                         return(
--- src/Exercise.js
+++ src/Exercise.js
@@ -5,24 +5,24 @@ const Exercise = () => {
     const weekArr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     return (
         <div>
-            <div className="pa3 pa5-ns">
-                <h4 className="f6 fw6">amount of exercise:</h4>
-                <dl className="f6 lh-title mv2">
-                    <dt className="dib b">Low:</dt>
-                    <dd className="dib ml0 gray">less than 12 sets of workout, or less than 0.5hr cardio.</dd>
-                </dl>
-                <dl className="f6 lh-title mv2">
-                    <dt className="dib b">Middle:</dt>
-                    <dd className="dib ml0 gray">15-20 sets workout, or 1hr cardio.</dd>
-                </dl>
-                <dl className="f6 lh-title mv2">
-                    <dt className="dib b">High:</dt>
-                    <dd className="dib ml0 gray">25 sets workout, or more than 1.5hr cardio.</dd>
-                </dl>
-            </div>
-
             <fieldset id="favorite_movies" className="bn">
                 <legend className="fw7 f4 ">choose your amount of exercise this week</legend>
+    
+                <div className="">
+                    <dl className="f6 lh-title mv2">
+                        <dt className="dib b">Low:</dt>
+                        <dd className="dib ml0 mid-gray">less than 12 sets of workout, or less than 0.5hr cardio.</dd>
+                    </dl>
+                    <dl className="f6 lh-title mv2">
+                        <dt className="dib b">Middle:</dt>
+                        <dd className="dib ml0 mid-gray">15-20 sets workout, or 1hr cardio.</dd>
+                    </dl>
+                    <dl className="f6 lh-title mv2">
+                        <dt className="dib b">High:</dt>
+                        <dd className="dib ml0 mid-gray">25 sets workout, or more than 1.5hr cardio.</dd>
+                    </dl>
+                </div>
+
                 <div>
                     {
                         weekArr.map(item => {
