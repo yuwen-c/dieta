fbe08c1d886ab05e40cbb1c607375fd9850e2a08
marine <marine@MarinedeiMac.local>
2020-08-24 14:48:02 +0800

    Updates

---
+++ precache-manifest.0506a957abaa8ab0bac67b68298b44ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d103a4cf0ceffe1b01330d7d6e2665",
+    "url": "/dieta/index.html"
+  },
+  {
+    "revision": "d288b3e53d2be0d08b88",
+    "url": "/dieta/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2917165c31dc2af3081b",
+    "url": "/dieta/static/css/main.921e8a96.chunk.css"
+  },
+  {
+    "revision": "d288b3e53d2be0d08b88",
+    "url": "/dieta/static/js/2.b92325b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dieta/static/js/2.b92325b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2917165c31dc2af3081b",
+    "url": "/dieta/static/js/main.c04b5736.chunk.js"
+  },
+  {
+    "revision": "a7f1ad9d8753ebe0588d",
+    "url": "/dieta/static/js/runtime-main.26a46b36.js"
+  }
+]);
