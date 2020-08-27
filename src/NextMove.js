c7b56c616ff7d906e035b0b214fc4d9eb5a3be0c
marine <marine@MarinedeiMac.local>
2020-08-27 18:10:42 +0800

    add description on speed up and slow down buttons

--- src/NextMove.js
+++ src/NextMove.js
@@ -15,28 +15,31 @@ const NextMove = ({onModifyClick, modifyState}) => {
                   <dd className="dib ml0 near-gray">Will you expect more stress (from work, live...)next week?</dd><br/>
                 </dl>
                 <div className="pv2">
-                    <input 
-                    className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                    type="submit" 
-                    value="Maintain Current Rate"
-                    // onClick={onBMRCalculate}
-                    />
+                  <input 
+                  className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                  type="submit" 
+                  value="Maintain Current Rate"
+                  // onClick={onBMRCalculate}
+                  />
+                  <small id="name-desc" className="f6 black-60 db mb2">Maintain the current deficit.</small>
                 </div>
                 <div className="pv2">
-                    <input 
-                    className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                    type="submit" 
-                    value="Speed Up"
-                    onClick={onModifyClick}
-                />
+                  <input 
+                  className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                  type="submit" 
+                  value="Speed Up"
+                  onClick={onModifyClick}
+                  />
+                  <small id="name-desc" className="f6 black-60 db mb2">Enlarge calorie deficit, eat less.</small>
                 </div>
                 <div className="pv2">
-                    <input 
-                    className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                    type="submit" 
-                    value="Slow Down"
-                    onClick={onModifyClick}
-                    />
+                  <input 
+                  className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                  type="submit" 
+                  value="Slow Down"
+                  onClick={onModifyClick}
+                  />
+                  <small id="name-desc" className="f6 black-60 db mb2">Minimize calorie deficit, eat more.</small>
                 </div>
                 <ModifyOptions
                   modifyState={modifyState}
