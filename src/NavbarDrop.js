5836600b95ece9fccc57be31d398773750016bb8
marine <marine@MarinedeiMac.local>
2020-09-07 18:24:56 +0800

    nav

--- src/NavbarDrop.js
+++ src/NavbarDrop.js
@@ -26,7 +26,6 @@ const NavbarDrop = ({ isSign, name, onRouteChange }) => {
                 onClick={() => {onRouteChange('rate')}}>
                 Next Move</NavDropdown.Item>
               </NavDropdown>
-                {/* 這邊的Nav裡面加item-end, 大螢幕時不會往右移，小螢幕時，下拉選單裡面的sign會跑到右邊 */}
             </Nav>
               { 
                 !isSign ?  
