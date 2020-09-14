6b8174ce9bf033157a7c98d3aee013e815a14002
yuwenGithub <yuwen.azulejos@gmail.com>
2020-09-14 10:42:49 +0800

    change to object format

--- src/explanation.js
+++ src/explanation.js
@@ -1,27 +1,37 @@
-export const titleY = [
-    'Why use this?',
-    'How it works?',
-    'Nutrition',
-    'Weekly adjustment'
+export const howItWorks = [
+    {
+        title: 'Why use this?',
+        description: 'A weekly plan helps you loss weight easily!'
+    },
+    {
+        title: 'How it works?',
+        description: 'Based on your weight, your life style, and the speed of losing weight, calculate the daily calorie.'
+    },
+    {
+        title: 'Nutrition',
+        description: 'A high level protein is needed to prevent a muscle losing. Also the carbohydrate is remained to maintain our body function.',
+    },
+    {
+        title: 'Weekly adjustment',
+        description: 'According to your losing rate, life style, tiredness to decide the next move.'
+    },
 ]
 
-export const descriptionY = [
-    'A weekly plan helps you loss weight easily!',
-    'Based on your weight, your life style, and the speed of losing weight, calculate the daily calorie.',
-    'A high level protein is needed to prevent a muscle losing. Also the carbohydrate is remained to maintain our body function.',
-    'According to your losing rate, life style, tiredness to decide the next move.'
-]
-
-export const titleB = [
-    'Getting start',
-    'How can I know the amount?',
-    'Check my last record',
-    'Next move?'
-]
-
-export const descriptionB =[
-    'First step, enter your body weight and estimate your daily activity and exercise amount.',
-    'number of steps, sets of work out, hours of housework can be an index, follow our instruction on the top of activity and exercise page.',
-    'Select latest result on menu',
-    'Enter your average weight this week and last week to get an advise!'
+export const HowToUse = [
+    {
+        title: 'Getting start',
+        description: 'First step, enter your body weight and estimate your daily activity and exercise amount.'   
+    },
+    {
+        title: 'How can I know the amount?',
+        description: 'number of steps, sets of work out, hours of housework can be an index, follow our instruction on the top of activity and exercise page.'   
+    },
+    {
+        title: 'Check my last record',
+        description: 'Select latest result on menu'   
+    },
+    {
+        title: 'Next move?',
+        description: 'Enter your average weight this week and last week to get an advise!'   
+    },
 ]
