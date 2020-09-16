24972fdd648061cc966dce20be1b7f7f2cc78f27
yuwenGithub <yuwen.azulejos@gmail.com>
2020-09-16 15:02:28 +0800

    add color and emoji

--- src/components/CalculationPath/Table/Table.js
+++ src/components/CalculationPath/Table/Table.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
- 
+import './Table.css';
 const TableExample = () => {
   return (
     <Table>
       <Thead>
-        <Tr className="stripe-dark">
+        <Tr className="white bg-mid-gray">
           <Th>Type</Th>
           <Th>Low</Th>
           <Th>Mideum</Th>
@@ -14,38 +14,38 @@ const TableExample = () => {
         </Tr>
       </Thead>
       <Tbody>
-        <Tr className="stripe-dark">
-          <Td>Steps</Td>
+        <Tr className="">
+          <Td><span className="pr2">🐾</span>Steps</Td>
           <Td>6,000</Td>
           <Td>10,000</Td>
           <Td>20,000</Td>
         </Tr>
-        <Tr className="stripe-dark">
-          <Td>Mounting</Td>
+        <Tr className="">
+          <Td><span className="pr2">⛰</span>Mounting</Td>
           <Td>0.5h</Td>
           <Td>1h</Td>
           <Td>2h</Td>
         </Tr>
-        <Tr className="stripe-dark">
-          <Td>Climbing stairs</Td>
+        <Tr className="">
+          <Td><span className="pr2">📶</span>Climbing stairs</Td>
           <Td>0.5h</Td>
           <Td>1h</Td>
           <Td>2h</Td>
         </Tr>
-        <Tr className="stripe-dark">
-          <Td>Walking</Td>
+        <Tr className="">
+          <Td><span className="pr2">🚶</span>Walking</Td>
           <Td>1h</Td>
           <Td>2h</Td>
           <Td>3h</Td>
         </Tr>
-        <Tr className="stripe-dark">
-          <Td>House cleaning</Td>
+        <Tr className="">
+          <Td><span className="pr2">🧹</span>House cleaning</Td>
           <Td>1h</Td>
           <Td>2h</Td>
           <Td>3h</Td>
         </Tr>
-        <Tr className="stripe-dark">
-          <Td>Biking</Td>
+        <Tr className="">
+          <Td><span className="pr2">🚴</span>Biking</Td>
           <Td>1h</Td>
           <Td>2h</Td>
           <Td>3h</Td>
