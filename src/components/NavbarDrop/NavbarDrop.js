3e6335e149367c04488606d842bea718981ac9f2
yuwenGithub <yuwen.azulejos@gmail.com>
2020-09-23 11:03:38 +0800

    add collapseOnSelect function and href, preventDefault

--- src/components/NavbarDrop/NavbarDrop.js
+++ src/components/NavbarDrop/NavbarDrop.js
@@ -1,48 +1,58 @@
 import React from 'react';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Navbar from 'react-bootstrap/Navbar';
 // from https://react-bootstrap.github.io/components/navbar/
 
 const NavbarDrop = ({ isSignIn, onRouteChange, getResult }) => {
     return(
-        <Navbar bg="light" expand="lg">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Navbar.Brand className="grow pointer"
             onClick={() => {onRouteChange('home')}}          
            >Dieta</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">            
-              <NavDropdown title="Menu" id="basic-nav-dropdown"> 
-              <NavDropdown.Item 
-                onClick={() => {onRouteChange('howItWorks')}}
-                >How it works</NavDropdown.Item>
+              <NavDropdown title="Menu" id="collasible-nav-dropdown"> 
                 <NavDropdown.Item 
-                onClick={() => {onRouteChange('calculation')}}
-                >Calculation</NavDropdown.Item>
+                  href="#howItWorks" 
+                  onClick={(event) => {onRouteChange('howItWorks'); event.preventDefault()}}
+                  // onClick={() => {onRouteChange('howItWorks')}}                  
+                  >How it works</NavDropdown.Item>
                 <NavDropdown.Item 
-                onClick={getResult}
-                >Latest Result</NavDropdown.Item>
+                  href="#calculation" 
+                  onClick={(event) => {onRouteChange('calculation'); event.preventDefault()}}
+                  >Calculation</NavDropdown.Item>
                 <NavDropdown.Item 
-                onClick={() => {onRouteChange('nextMove')}}>
-                Next Move</NavDropdown.Item>
-              </NavDropdown>
+                  href="#getResult"
+                  onClick={(event) => {getResult(); event.preventDefault()}}
+                  >Latest Result</NavDropdown.Item>
+                <NavDropdown.Item 
+                  href="#nextMove" 
+                  onClick={(event) => {onRouteChange('nextMove'); event.preventDefault()}}>
+                  Next Move</NavDropdown.Item>
+                </NavDropdown>
             </Nav>
               { 
                 !isSignIn ?  
               <Nav > 
                 <Nav.Link
-                onClick={() => {onRouteChange('signin')}}
+                href="#signin"
+                onClick={(event) => {onRouteChange('signin'); event.preventDefault()}}
                 >Sign In</Nav.Link>
                 <Nav.Link
-                onClick={() => {onRouteChange('signup')}}
+                href="#signup"                
+                onClick={(event) => {onRouteChange('signup'); event.preventDefault()}}
                 >Sign Up</Nav.Link>
               </Nav>
-              :             
-              <Nav.Link
-              onClick={() => {onRouteChange('signin')}}
-              >Sign Out</Nav.Link>             
+              :  
+              <Nav>
+                <Nav.Link
+                href="signin"
+                onClick={(event) => {onRouteChange('signin'); event.preventDefault()}}
+                >Sign Out</Nav.Link>                
+              </Nav>                      
               }          
           </Navbar.Collapse>
         </Navbar>
