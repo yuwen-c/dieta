04b52117c67adeb7a22bd10aee209310862890da
yuwenGithub <yuwen.azulejos@gmail.com>
2020-11-10 12:34:42 +0800

    Updates

---
+++ precache-manifest.ec090c0a9e5036ad8b27b25c5f0e45b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca9dc54d2afb7fa8f93ed9c57c90fa3",
+    "url": "/dieta/index.html"
+  },
+  {
+    "revision": "16d3e273e98f0e97492c",
+    "url": "/dieta/static/css/2.240481f6.chunk.css"
+  },
+  {
+    "revision": "0c409f4473c94fa0cee8",
+    "url": "/dieta/static/css/main.ab450499.chunk.css"
+  },
+  {
+    "revision": "16d3e273e98f0e97492c",
+    "url": "/dieta/static/js/2.e49e1c0f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dieta/static/js/2.e49e1c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c409f4473c94fa0cee8",
+    "url": "/dieta/static/js/main.025625b2.chunk.js"
+  },
+  {
+    "revision": "a7f1ad9d8753ebe0588d",
+    "url": "/dieta/static/js/runtime-main.26a46b36.js"
+  }
+]);
