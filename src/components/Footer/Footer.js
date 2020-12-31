8cf06d7783a8e8525c91ae004066dc2e0335a6da
yuwen-c <yuwen.azulejos@gmail.com>
2020-12-31 17:15:17 +0800

    add email in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
               <span className="f6 ml1 ">yuwen-c</span>
             </a>
             
-            <a className="link black-80 dim inline-flex items-center ma2 tc br2 pa2" href="mailto: http://codepen.io" title="Gmail">
+            <a className="link black-80 dim inline-flex items-center ma2 tc br2 pa2" href="mailto:yuwen.azulejos@gmail.com" title="Gmail">
               <svg className="dib w1" fill="currentColor" xmlns="http://www.w3.org/2000/svg" 
               viewBox="0 0 24 24" fillRule="evenodd" clipRule="evenodd" strokeLinejoin="round" strokeMiterlimit="1.414" >
               <path d="M24 4.5v15c0 .85-.65 1.5-1.5 1.5H21V7.387l-9 6.463-9-6.463V21H1.5C.649 21 0 20.35 0 19.5v-15c0-.425.162-.8.431-1.068C.7 3.16 1.076 3 1.5 3H2l10 7.25L22 3h.5c.425 0 .8.162 1.069.432.27.268.431.643.431 1.068z"/></svg>     
