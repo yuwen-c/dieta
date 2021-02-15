614e5d62b91cb3cb7fe4d455fcd3974d39298973
yuwen-c <yuwen.azulejos@gmail.com>
2021-02-15 18:58:16 +0800

    add log in button on home page, adjust space

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,20 +6,23 @@ const Home = ({onRouteChange, onGuestLogin}) => {
 
     return(
         <div className="mt5">
-            <article className="mw7 center ph3 ph5-ns tc br2 pv5 bg-washed-green dark-green mb5">
+            <article className="mw7 center ph3 ph5-ns tc br2 pv4 bg-washed-green dark-green mb4">
               <h1 className="fw6 f3 f2-ns lh-title mt0 mb3">
                 {t('home.slogan')}
               </h1>
               <h2 className="fw4 f4 lh-copy mt0 mb3 ph4">
                 {t('home.description')}
               </h2>
-              <div className="pt3">
+              <div className="pt2">
+                <div className="f5 br-pill bg-dark-green no-underline washed-green ba b--dark-green grow pv2 ph3 dib mr1"
+                onClick={() => {onRouteChange('signin')}} >
+                {t('home.signin')}</div>
                 <div className="f5 br-pill bg-dark-green no-underline washed-green ba b--dark-green grow pv2 ph3 dib mr1"
                 onClick={() => {onRouteChange('howItWorks')}} >
-                {t('home.option')}</div>
+                {t('home.more')}</div>
                 <div className="f5 br-pill bg-dark-green no-underline washed-green ba b--dark-green grow pv2 ph3 dib ml1 animate__animated animate__pulse animate__infinite"
                 onClick={onGuestLogin} >
-                {t('sign_up.try')}</div>
+                {t('home.tryit')}</div>
               </div>
             </article>            
         </div>
