1b4b7d7bacee307f2cc18909f92b5a9d30808a9d
yuwen-c <yuwen.azulejos@gmail.com>
2021-03-24 18:24:49 +0800

    finish translation of guidePage

--- src/components/GuidePage/GuidePage.js
+++ src/components/GuidePage/GuidePage.js
@@ -27,58 +27,58 @@ const GuidePage = () => {
             </nav>
             <header className="pv4">
                 <h4 className="f3 fw7 tracked lh-title mt0 mb3">
-                    <Emoji emoji="🎯" label="bullseye"/>不用註冊即可試用！</h4>
-                <h4 className="f3 fw6 lh-title mt0">首頁Dieta→試用(可體驗部分功能)，喜歡再註冊。</h4>
-                <h4 className="f3 fw6 lh-title mt0">或是直接前往選單→註冊頁面填寫資料。</h4>
+                    <Emoji emoji="🎯" label="bullseye"/>{t('guidePage.cta')}</h4>
+                <h4 className="f3 fw6 lh-title mt0">{t('guidePage.tryIt')}</h4>
+                <h4 className="f3 fw6 lh-title mt0">{t('guidePage.signUp')}</h4>
             </header>
             <section ref={firstTime} className="mb5">
-            <h4 className="f3 fw6 lh-title mt0">第一次使用 <Emoji emoji="☝" label="index pointing up"/>：</h4>
+            <h4 className="f3 fw6 lh-title mt0">{t('guidePage.nav1st')}<Emoji emoji="☝" label="index pointing up"/>：</h4>
             <ul>
-                <li>選單→目錄→開始減脂</li>
-                <li>輸入體重，選擇熱量赤字。</li>
+                <li>{t('guidePage.1stRoute')}</li>
+                <li>{t('guidePage.enter')}</li>
             </ul>
             <img src={photo_calculate} className="w-100 f5 measure" alt="Dieta_calculate"/>
 
 
             <ul>
-                <li>輸入本週的活動量與運動量等級。低/中/高等級可參考畫面上方的表格。</li>
+                <li>{t('guidePage.selectLevel')}</li>
             </ul>
             <img src={photo_activity} className="w-100 f5 measure" alt="Dieta_activity"/>
             
             <ul>
-                <li>最後算出本週的營養素及熱量建議。</li>
+                <li>{t('guidePage.nutrition')}</li>
             </ul>
             <img src={photo_nutrition} className="w-100 f5 measure" alt="Dieta_nutrition"/>
 
-            <p className="b"> <Emoji emoji="👑" label="crown"/>小技巧：隨時可從 選單→目錄→上次計算結果 叫出紀錄<Emoji emoji="🤙" label="call me hand"/></p>
+            <p className="b"> <Emoji emoji="👑" label="crown"/>{t('guidePage.tipResult')}<Emoji emoji="🤙" label="call me hand"/></p>
             </section>
 
             <section ref={secondTime} className="mb5">
-            <h4 className="f3 fw6 lh-title mt0">減脂進入第二週以上<Emoji emoji="✌" label="victory hand"/>：</h4>
+            <h4 className="f3 fw6 lh-title mt0">{t('guidePage.nav2nd')}<Emoji emoji="✌" label="victory hand"/>：</h4>
             <ul>
-                <li>此功能僅供完成註冊，且已完成「第一次計算」的使用者。</li>
-                <li>減脂進行一週後，前往選單→目錄→減脂期間。</li>
-                <li>填入本週，及上週的體重平均，送出後即可獲得減脂建議。</li>
-                <li>根據建議，及評估自身狀況，決定下週的減脂速度是否要調整。熱量赤字增加，表示減脂速度加快，減少，表示放慢減脂。</li>
+                <li>{t('guidePage.restriction')}</li>
+                <li>{t('guidePage.2ndRoute')}</li>
+                <li>{t('guidePage.getAdvice')}</li>
+                <li>{t('guidePage.adjustment')}</li>
             </ul>
-            <p className="b">這個步驟是根據「首次計算」的熱量結果來進行調整，才符合結果導向的意義。</p>
+            <p className="b"><Emoji emoji="🎯" label="bullseye"/>{t('guidePage.resultOriented')}</p>
             <img src={photo_nextmove} className="w-100 f5 measure" alt="Dieta_nextmove"/>
             
             <ul>
-                <li>接著像上週一樣，選擇活動量及運動量等級。</li>
+                <li>{t('guidePage.selectLevelAgain')}</li>
             </ul>
-            <p className="b"><Emoji emoji="👑" label="crown"/>小技巧：可直接點選下載上週紀錄按鈕，載入後也可再修改。</p>
+            <p className="b"><Emoji emoji="👑" label="crown"/>{t('guidePage.tipLoad')}</p>
 
             <ul>
-                <li>計算出下週的營養素及熱量建議。</li>
+                <li>{t('guidePage.nutrition')}</li>
             </ul>
             </section>
 
             <section ref={newRound} className="mb5">
-            <h4 className="f3 fw6 lh-title mt0">開始新一輪減脂<Emoji emoji="🤟" label="love-you gesture"/>：</h4>
+            <h4 className="f3 fw6 lh-title mt0">{t('guidePage.navAgain')}<Emoji emoji="🤟" label="love-you gesture"/>：</h4>
             <ul>
-                <li>請從第一步驟：選單→目錄→開始減脂計算重新開始。</li>
-                <li>進入第二週時再到選單→目錄→減脂期間。</li>
+                <li>{t('guidePage.againRoute')}</li>
+                <li>{t('guidePage.againDuring')}</li>
             </ul>
             </section>
         </article>
