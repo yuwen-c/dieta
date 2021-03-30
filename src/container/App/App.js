94dec13aceea640c2149e34686b1f8893f427db6
yuwen-c <yuwen.azulejos@gmail.com>
2021-03-30 16:17:24 +0800

    fix float of 1.6 times protein

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -343,7 +343,7 @@ class App extends Component{
     const {name, email, weight, deficit} = this.state.user;
     const {activity, exercise, modifyDeficit} = this.state; 
     
-    const protein = weight * 1.6; // fix protein to 1.6 times weight
+    const protein = parseInt(weight * 1.6); // fix protein to 1.6 times weight
     const oil = weight * 1; // fix oil to 1 time weight
 
     let dailyCalorie = [];
