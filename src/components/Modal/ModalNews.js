11ef879c5d2e1bc264d2a88ad9514cd1101abccf
yuwen-c <yuwen.azulejos@gmail.com>
2021-04-15 18:00:15 +0800

    change modalNews to map, will only need to change newsCount, and edit locales files when adding news

--- src/components/Modal/ModalNews.js
+++ src/components/Modal/ModalNews.js
@@ -3,17 +3,24 @@ import { useTranslation } from 'react-i18next';
 
 const ModalNews = () => {
     const { t } = useTranslation();
+    const newsCount = 2; // how many news there. have to change when adding news
+    const newsArray = Array.from(Array(newsCount).keys()) // create an array with length of newsCount
 
     return (
         <ul className="list tl ph2">
-            <li>
-                <span className="dib pr1 v-top">#</span>
-                <span className="dib w-90 v-top">{t('footer.news1')}</span>
-            </li>
-            <li>
-                <span className="dib pr1 v-top">#</span>
-                <span className="dib w-90 v-top">{t('footer.news2')}</span>
-            </li>
+        {
+            newsArray.map((item, index) => {
+                return(
+                    <div
+                    key={item}>
+                        <li>
+                            <span className="dib pr1 v-top">#</span>
+                            <span className="dib w-90 v-top">{t(`footer.news${index+1}`)}</span>
+                        </li>
+                    </div>
+                )
+            })
+        }
         </ul>
     )
 }
