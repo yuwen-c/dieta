9dadcac7144b22e281d7e5cbb5a15addf26a2b7d
yuwen <yuwen.azulejos@gmail.com>
2022-09-07 17:04:18 +0800

    chore: remove log

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,101 +1,107 @@
-import React, { Component } from 'react';
-import { withTranslation } from 'react-i18next';
+import React, { Component } from "react";
+import { withTranslation } from "react-i18next";
 
-class SignIn extends Component{
-    constructor(){
-        super();
-        this.state = {
-            email: '', 
-            password: '',
-            message: ''
-        }
+class SignIn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      message: "",
+    };
+  }
+
+  onEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  onSignIn = () => {
+    const { email, password } = this.state;
+    if (email && password) {
+      fetch("https://gentle-badlands-25513.herokuapp.com/signin", {
+        // fetch('http://localhost:3000/signin', {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(this.state),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          if (result.name) {
+            this.props.refreshWholeUser(result);
+            this.props.onIsSignIn();
+            this.props.onRouteChange("howItWorks");
+          } else {
+            let errMes = this.props.t("sign_in.error_fail");
+            this.setState({ message: errMes });
+          }
+        })
+        .catch(console.log);
+    } else {
+      let errMes = this.props.t("sign_in.error_blank");
+      this.setState({ message: errMes });
     }
+  };
 
-    onEmailChange = (event) => {
-        this.setState({email : event.target.value})
-    }
+  componentDidMount = () => {
+    fetch("https://gentle-badlands-25513.herokuapp.com/")
+      .then((response) => response.json())
+      .then();
+  };
 
-    onPasswordChange = (event) => {
-        this.setState({password : event.target.value})
-    }
-
-    onSignIn = () => {
-        const {email, password} = this.state;
-        if(email && password){
-            fetch('https://gentle-badlands-25513.herokuapp.com/signin', {
-            // fetch('http://localhost:3000/signin', {
-                method: 'post',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(this.state)
-            })
-            .then(response => response.json())
-            .then(result => {
-                if(result.name){
-                    this.props.refreshWholeUser(result);
-                    this.props.onIsSignIn();
-                    this.props.onRouteChange('howItWorks');
-                }
-                else{
-                    let errMes = this.props.t('sign_in.error_fail');
-                    this.setState({message : errMes});
-                }
-            })
-            .catch(console.log);
-        }
-        else{
-            let errMes = this.props.t('sign_in.error_blank');
-            this.setState({message : errMes});
-        }
-    }
-
-    componentDidMount = () => {
-        fetch('https://gentle-badlands-25513.herokuapp.com/')
-        .then(response => response.json())
-        .then(result => console.log(result))
-    }
-
-    render(){
-        return(
-            <div>
-                <div className="pa4 black-80 ">
-                    <div className="measure center">
-                        <fieldset id="sign_in" className="ba b--transparent ph0 mh0">
-                            <legend className="f4 fw6 ph0 mh0">{this.props.t("sign_in.title")}</legend>
-                            <div className="mt3">
-                               <label className="db fw6 lh-copy f6" htmlFor="email-address">{this.props.t("sign_in.email")}</label>
-                               <input 
-                                className="pa2 input-reset ba hover-bg-black hover-white w-100" 
-                                type="email" name="email-address"  id="email-address"
-                                onChange={this.onEmailChange}
-                               />
-                            </div>
-                            <div className="mv3">
-                              <label className="db fw6 lh-copy f6" htmlFor="password">{this.props.t("sign_in.password")}</label>
-                              <input 
-                                className="b pa2 input-reset ba hover-bg-black hover-white w-100" 
-                                type="password" name="password"  id="password"
-                                onChange={this.onPasswordChange}
-                              />
-                            </div>
-                        </fieldset>
-                        <div className="">
-                            <input 
-                            className="b ph3 pv2 input-reset ba b--black bg-white grow pointer f6 dib" 
-                            type="submit" 
-                            value={this.props.t("sign_in.button")}
-                            onClick={this.onSignIn}
-                            />
-                        </div>
-                        <div className="lh-copy mt3">
-                            <p 
-                            className="f5 link dark-pink db"
-                            >{this.state.message}</p>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div>
+        <div className="pa4 black-80 ">
+          <div className="measure center">
+            <fieldset id="sign_in" className="ba b--transparent ph0 mh0">
+              <legend className="f4 fw6 ph0 mh0">
+                {this.props.t("sign_in.title")}
+              </legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  {this.props.t("sign_in.email")}
+                </label>
+                <input
+                  className="pa2 input-reset ba hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  {this.props.t("sign_in.password")}
+                </label>
+                <input
+                  className="b pa2 input-reset ba hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <input
+                className="b ph3 pv2 input-reset ba b--black bg-white grow pointer f6 dib"
+                type="submit"
+                value={this.props.t("sign_in.button")}
+                onClick={this.onSignIn}
+              />
             </div>
-        )
-    }
+            <div className="lh-copy mt3">
+              <p className="f5 link dark-pink db">{this.state.message}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 // export default SignIn;
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,151 +1,159 @@
-import React, { Component } from 'react';
-import { withTranslation } from 'react-i18next';
+import React, { Component } from "react";
+import { withTranslation } from "react-i18next";
 
-class SignUp extends Component{
-    constructor(){
-        super();
-        this.state = {
-            name: '',
-            email: '',
-            password: '',
-            message: ''
-        }
-    }
+class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      message: "",
+    };
+  }
 
-    onNameChange = (event) => {
-        this.setState({name : event.target.value})
-    }
+  onNameChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
 
-    onEmailChange = (event) => {
-        this.setState({email : event.target.value})
-    }
+  onEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
 
-    onPasswordChange = (event) => {
-        this.setState({password : event.target.value})
-    }
+  onPasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
 
-    // save new user data to database, return a promise
-    onSignUpFetch = (name, email, password) => {
-        const user = {
-            name: name, 
-            email: email,
-            password: password
-        }
-        return fetch('https://gentle-badlands-25513.herokuapp.com/signup', {
-        // return fetch('http://localhost:3000/signup', {
-            method: 'post', 
-            headers: {'Content-Type' : 'application/json'},
-            body : JSON.stringify(user)
+  // save new user data to database, return a promise
+  onSignUpFetch = (name, email, password) => {
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+    };
+    return fetch("https://gentle-badlands-25513.herokuapp.com/signup", {
+      // return fetch('http://localhost:3000/signup', {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .catch(console.log);
+  };
+
+  // sign up, 2 conditions, normal user and guest
+  onSignUp = () => {
+    const { name, email, password } = this.state;
+    if (name && email && password) {
+      if (this.props.name !== "Guest") {
+        // normal user sign up
+        this.onSignUpFetch(name, email, password)
+          .then(async (result) => {
+            if (result.name) {
+              this.props.refreshWholeUser(result); // refresh user state
+              let signup = await this.props.onIsSignIn(); // change signIn state
+              this.props.onRouteChange("calculation"); // to calculation page
+            } else {
+              let errMes = this.props.t("sign_up.error.fail");
+              this.setState({ message: errMes });
             }
-        )
-        .then(response => response.json())  
-        .catch(console.log)
-    }
-
-    // sign up, 2 conditions, normal user and guest
-    onSignUp = () => {
-        const {name, email, password} = this.state;
-        if(name && email && password){
-            if(this.props.name !== "Guest"){ // normal user sign up
-                this.onSignUpFetch(name, email, password) 
-                .then(async result => {
-                    if(result.name){
-                        this.props.refreshWholeUser(result);   // refresh user state
-                        let signup = await this.props.onIsSignIn();  // change signIn state
-                        this.props.onRouteChange('calculation');   // to calculation page
-                    }
-                    else{
-                        let errMes = this.props.t("sign_up.error.fail");
-                        this.setState({message : errMes});
-                    }     
-                })
-                .catch(console.log)
+          })
+          .catch(console.log);
+      } else {
+        // guest user sign up
+        this.onSignUpFetch(name, email, password) // save new user data
+          .then((result) => {
+            if (result.name) {
+              const { deficit, dailyCalorie, dailyCarbon } = this.props; // save calculation result to database
+              this.props.onSaveCalculation(
+                result.email,
+                deficit,
+                dailyCalorie,
+                dailyCarbon
+              );
+              this.props.refreshPartialUser(result); // only refresh user name and email state
+              this.props.onRouteChange("result"); // to result page
+            } else {
+              let errMes = this.props.t("sign_up.error.fail");
+              this.setState({ message: errMes });
             }
-            else{                            // guest user sign up
-                this.onSignUpFetch(name, email, password) // save new user data
-                .then(result => {
-                    if(result.name){ 
-                        const {deficit, dailyCalorie, dailyCarbon} = this.props;  // save calculation result to database
-                        this.props.onSaveCalculation(result.email, deficit, dailyCalorie, dailyCarbon);
-                        this.props.refreshPartialUser(result); // only refresh user name and email state
-                        this.props.onRouteChange('result'); // to result page
-
-                    }
-                    else{
-                        let errMes = this.props.t("sign_up.error.fail");
-                        this.setState({message : errMes});
-                    }     
-                })
-                .catch(console.log)          
-            }
-        }
-        else{
-            let errMes = this.props.t('sign_up.error_blank');
-            this.setState({message : errMes})
-        }
+          })
+          .catch(console.log);
+      }
+    } else {
+      let errMes = this.props.t("sign_up.error_blank");
+      this.setState({ message: errMes });
     }
+  };
 
-    componentDidMount = () => {
-        fetch('https://gentle-badlands-25513.herokuapp.com/')
-        .then(response => response.json())
-        .then(result => console.log(result))
-    }
+  componentDidMount = () => {
+    fetch("https://gentle-badlands-25513.herokuapp.com/")
+      .then((response) => response.json())
+      .then();
+  };
 
-    render(){
-        return(
-            <div>
-                <div className="pa4 black-80">
-                    <div className="measure center">
-                        <fieldset id="sign_in" className="ba b--transparent ph0 mh0">
-                            <legend className="f4 fw6 ph0 mh0 flex">
-                                <div>
-                                    {this.props.t('sign_up.title')}
-                                </div>
-                            </legend>
-                            <div className="mt3">
-                               <label className="db fw6 lh-copy f6" htmlFor="name">{this.props.t('sign_up.name')}</label>
-                               <input 
-                                className="pa2 input-reset ba hover-bg-black hover-white w-50" 
-                                type="name" name="name"  id="name"
-                                onChange={this.onNameChange}
-                               />
-                            </div>
-                            <div className="mt3">
-                               <label className="db fw6 lh-copy f6" htmlFor="email-address">{this.props.t('sign_up.email')}</label>
-                               <input 
-                                className="pa2 input-reset ba hover-bg-black hover-white w-100" 
-                                type="email" name="email-address"  id="email-address"
-                                onChange={this.onEmailChange}
-                               />
-                            </div>
-                            <div className="mv3">
-                              <label className="db fw6 lh-copy f6" htmlFor="password">{this.props.t('sign_up.password')}</label>
-                              <input 
-                               className="b pa2 input-reset ba hover-bg-black hover-white w-100" 
-                               type="password" name="password"  id="password"
-                               onChange={this.onPasswordChange}
-                               />
-                            </div>
-                        </fieldset>
-                        <div className="">
-                            <input 
-                            className="b ph3 pv2 input-reset ba b--black bg-white grow pointer f6 dib" 
-                            type="submit" 
-                            value={this.props.t('sign_up.button')}
-                            onClick={this.onSignUp}
-                            />
-                        </div>
-                        <div className="lh-copy mt3">
-                            <p 
-                            className="f5 link dark-pink db"
-                            >{this.state.message}</p>
-                        </div>
-
-                    </div>
-                </div>
+  render() {
+    return (
+      <div>
+        <div className="pa4 black-80">
+          <div className="measure center">
+            <fieldset id="sign_in" className="ba b--transparent ph0 mh0">
+              <legend className="f4 fw6 ph0 mh0 flex">
+                <div>{this.props.t("sign_up.title")}</div>
+              </legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="name">
+                  {this.props.t("sign_up.name")}
+                </label>
+                <input
+                  className="pa2 input-reset ba hover-bg-black hover-white w-50"
+                  type="name"
+                  name="name"
+                  id="name"
+                  onChange={this.onNameChange}
+                />
+              </div>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  {this.props.t("sign_up.email")}
+                </label>
+                <input
+                  className="pa2 input-reset ba hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  {this.props.t("sign_up.password")}
+                </label>
+                <input
+                  className="b pa2 input-reset ba hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <input
+                className="b ph3 pv2 input-reset ba b--black bg-white grow pointer f6 dib"
+                type="submit"
+                value={this.props.t("sign_up.button")}
+                onClick={this.onSignUp}
+              />
             </div>
-        )
-    }
+            <div className="lh-copy mt3">
+              <p className="f5 link dark-pink db">{this.state.message}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 // export default SignUp;
